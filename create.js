bbe3e7506163e28597b7df8e7361ccb1d109111a
Ruoya Acelyn Ding <Acelyn@appledeMacBook-Pro-2.local>
2020-02-23 17:14:43 -0500

    try create

--- create.js
+++ create.js
@@ -9,11 +9,11 @@ const file = readline.createInterface({
 connect();
 
 const voters = []
-fs.readline('line', function(err,line){
+fs.on('line', function(err,line){
   const data = line.spit(',');
   voters.push(new Voter({first_name: data[0],
                         last_name: data[1],
                         zip: data[2],
                         history: data[3]
                       }));
-    });  
+    });
