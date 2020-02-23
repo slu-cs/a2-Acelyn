cc9913963c222ea721c4663a849ff403829436da
Ruoya Acelyn Ding <Acelyn@appledeMacBook-Pro-2.local>
2020-02-23 18:16:44 -0500

    without temp array

--- query.js
+++ query.js
@@ -15,8 +15,11 @@ const queries = [
 
   // What are names in alphabetical order?
 
-  Voter.find({zip: 13617}).then(result =>result.map(r=> tem.push(r)))
-  .then(tem => (tem.length))
+  Voter.find({zip: 13617}).then(result =>(result.length))
+    //result.map(r=> tem.push(r)))
+//  .then(tem => (tem.length))
+
+  //Voter.find({first_name: Starr}).then
 
         ]
 
@@ -38,8 +41,8 @@ const queries = [
 // Run the queries in parallel
 Promise.all(queries)
   .then(function(queries) {
-    console.log(queries[0], `registered voters live in the Canton zip code 13617`);
-    //console.log('Started most recently: ', results[1].map(p => p.name));
+    console.log('How many registered voters live in the Canton zip code (13617)?', queries[0]);
+    console.log('What are the full names of all the registered voters whose first-name is STARR?', queries[1]);
   //  console.log('Started in 2003: ', results[2].map(p => p.name));
   //  console.log('Teaches 362: ', results[3].map(p => p.name));
   //  console.log('Distinct ranks: ', results[4]);
