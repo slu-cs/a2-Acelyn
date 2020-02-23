def2ff6a7e64fe36f1261fb58c7c57e0ff706c53
Ruoya Acelyn Ding <Acelyn@appledeMacBook-Pro-2.local>
2020-02-23 18:24:54 -0500

    q2

--- query.js
+++ query.js
@@ -33,7 +33,7 @@ const queries = [
 Promise.all(queries)
   .then(function(queries) {
     console.log('How many registered voters live in the Canton zip code (13617)?', queries[0]);
-    console.log('What are the full names of all the registered voters whose first-name is STARR?', queries[1]);
+    console.log('What are the full names of all the registered voters whose first-name is STARR?', queries[1]map(p => p.first_name),queries[1]map(p => p.last_name));
   //  console.log('Started in 2003: ', results[2].map(p => p.name));
   //  console.log('Teaches 362: ', results[3].map(p => p.name));
   //  console.log('Distinct ranks: ', results[4]);
