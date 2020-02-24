a877bf0024ac9c2ec8bfa49f25d459861b5c7d31
Ruoya Acelyn Ding <Acelyn@appledeMacBook-Pro-2.local>
2020-02-23 20:21:45 -0500

    q5

--- query.js
+++ query.js
@@ -18,24 +18,11 @@ const queries = [
   Voter.find({history: /GE16/i}).then(result =>(result.length)),
 
   //What is the last-name that comes last in the county in alphabetical order?
-  Voter.find().sort('-last_name')
+  Voter.find().sort('-last_name'),
 
+  //How many zip codes does the county contain?
+  Voter.findOne().length
         ]
-
-
-
-  // Who started most recently?
-  //Voter.find().sort('-started').limit(1),
-
-  // Who started in 2003?
-//  Professor.find().where('started').equals(2003),
-
-  // Who teaches 362?
-//  Professor.find().where('courses').in(362),
-
-  // What are all the ranks?
-//  Professor.distinct('rank')
-//];
 
 // Run the queries in parallel
 Promise.all(queries)
@@ -44,6 +31,6 @@ Promise.all(queries)
     console.log('What are the full names of all the registered voters whose first-name is STARR: ', queries[1].map(p => p.first_name),queries[1].map(p => p.last_name));
     console.log('How many people voted in the 2016 general election (GE16)?: ', queries[2]);
     console.log('What is the last-name that comes last in the county in alphabetical order?: ', queries[3][0].last_name);
-  //  console.log('Distinct ranks: ', results[4]);
+    console.log('How many zip codes does the county contain: ', queries[4]);
     mongoose.connection.close();
   }).catch(error => console.error(error.stack));
