aa7ba69990826c502f5a43f8359a1c25f96dd121
Ruoya Acelyn Ding <Acelyn@appledeMacBook-Pro-2.local>
2020-02-23 20:02:29 -0500

    q3

--- query.js
+++ query.js
@@ -14,7 +14,7 @@ const queries = [
   Voter.find().where('first_name: ').equals('Starr'),
 
   //How many people voted in the 2016 general election (GE16)?
-  Voter.exists({history: /GE16/i}).then(result =>(result.length))
+  Voter.find({history: /GE16/i}).then(result =>(result.length))
 
   //What is the last-name that comes last in the county in alphabetical order?
 
