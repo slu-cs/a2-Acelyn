9d76ac1a244a38c7df5c8dcf20aae7ef56bf6847
Ruoya Acelyn Ding <Acelyn@appledeMacBook-Pro-2.local>
2020-02-23 20:05:35 -0500

    q4

--- query.js
+++ query.js
@@ -8,16 +8,17 @@ connect(); // To the database
 const queries = [
 
   //How many registered voters live in the Canton zip code (13617)?
-  Voter.find({zip: 13617}).then(result =>(result.length)),
+  Voter.find({zip: 13617}).length,
+  //then(result =>(result.length)),
 
   //What are the full names of all the registered voters whose first-name is STARR?
-  Voter.find().where('first_name: ').equals('Starr'),
+  Voter.find().where('first_name').equals('Starr'),
 
   //How many people voted in the 2016 general election (GE16)?
   Voter.find({history: /GE16/i}).then(result =>(result.length))
 
   //What is the last-name that comes last in the county in alphabetical order?
-
+  Voter.find().sort('-last_name')
 
         ]
 
@@ -42,7 +43,7 @@ Promise.all(queries)
     console.log('How many registered voters live in the Canton zip code (13617): ', queries[0]);
     console.log('What are the full names of all the registered voters whose first-name is STARR: ', queries[1].map(p => p.first_name),queries[1].map(p => p.last_name));
     console.log('How many people voted in the 2016 general election (GE16)?: ', queries[2]);
-    //console.log('What is the last-name that comes last in the county in alphabetical order?: ', results[3].map(p => p.name));
+    console.log('What is the last-name that comes last in the county in alphabetical order?: ', queries[3][0]);
   //  console.log('Distinct ranks: ', results[4]);
     mongoose.connection.close();
   }).catch(error => console.error(error.stack));
