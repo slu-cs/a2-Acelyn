11c908825ccf60c1c412300cc77b52cb2a034eea
Ruoya Acelyn Ding <Acelyn@appledeMacBook-Pro-2.local>
2020-02-23 19:56:14 -0500

    q3

--- query.js
+++ query.js
@@ -36,9 +36,9 @@ const queries = [
 // Run the queries in parallel
 Promise.all(queries)
   .then(function(queries) {
-    console.log('How many registered voters live in the Canton zip code (13617)?', queries[0]);
-    console.log('What are the full names of all the registered voters whose first-name is STARR?', queries[1].map(p => p.first_name),queries[1].map(p => p.last_name));
-  //  console.log('Started in 2003: ', results[2].map(p => p.name));
+    console.log('How many registered voters live in the Canton zip code (13617):', queries[0]);
+    console.log('What are the full names of all the registered voters whose first-name is STARR:', queries[1].map(p => p.first_name),queries[1].map(p => p.last_name));
+    console.log('How many people voted in the 2016 general election (GE16)?:', queries[3]);
   //  console.log('Teaches 362: ', results[3].map(p => p.name));
   //  console.log('Distinct ranks: ', results[4]);
     mongoose.connection.close();
